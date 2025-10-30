eb1670d349305e9b8bfe285c45bfd127c415c7e5
Eric467-Ben <beneric467@gmail.com>
2025-10-30 09:31:58 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,20 @@
-require('@nomiclabs/hardhat-ethers');
-require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [{ version: '0.8.19' }]
+    compilers: [{ version: "0.8.19" }],
   },
   networks: {
     hedera_testnet: {
-      url: process.env.HEDERA_RPC || 'https://testnet.hashgraph.rpc',
+      url: process.env.HEDERA_RPC || "https://testnet.hashgraph.rpc",
       chainId: Number(process.env.HEDERA_CHAIN_ID || 296),
-      accounts: process.env.HEDERA_PRIVATE_KEY ? [process.env.HEDERA_PRIVATE_KEY] : []
+      accounts: process.env.HEDERA_PRIVATE_KEY
+        ? [process.env.HEDERA_PRIVATE_KEY]
+        : [],
     },
-    localhost: { url: 'http://127.0.0.1:8545' }
+    localhost: { url: "http://127.0.0.1:8545" },
   },
-  mocha: { timeout: 300000 }
+  mocha: { timeout: 300000 },
 };
